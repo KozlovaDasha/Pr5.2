4a08eee96fe494e9ed78ed39621bb11bdd126609
KozlovaDasha <dashakozlova255@ukr.net>
2019-05-26 23:39:15 +0300

    Добавьте файлы проекта.

---
+++ Pr5.2/A.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr5
+{
+   
+    class A
+    {
+        public int a;
+        public int b;
+        public int C => a > b ? a - b : b - a;
+
+       
+        public A()
+        {
+            a = 10;
+            b = 15;
+        }
+
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
---
+++ Pr5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A First = new A(12, 34);
+            A First1 = new A();
+            B Second = new B(3, 1, 7);
+            B Second2 = new B(3);
+            Console.WriteLine(Second.C2);
+            Console.WriteLine(Second2.C2);
+            Console.ReadLine();
+        }
+    }
+}
